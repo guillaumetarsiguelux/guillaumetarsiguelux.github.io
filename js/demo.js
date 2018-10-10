39dca53d48e944e1762dedeee84c5e4640caa48f
Tarsi <guillaume.tarsi@gmail.com>
2018-10-10 23:33:00 +0200

    working demo

---
+++ js/demo.js
@@ -0,0 +1,22 @@
+function updateText() {
+	'use strict';
+	var i18n = $.i18n(), language, person, kittens, message, gender;
+
+	message = 'kitten';
+	language = $( '.language option:selected' ).val();
+
+	i18n.locale = language;
+	i18n.load( 'i18n/demo-' + i18n.locale + '.json', i18n.locale ).done(
+		function () {
+			var personName = $.i18n( person ), localizedMessage = $.i18n( message, personName);
+			$( '.result' ).text( localizedMessage ).prop( 'title', i18n.localize( message ) );
+		} );
+}
+// Enable debug
+$.i18n.debug = true;
+
+$( document ).ready( function ( $ ) {
+	'use strict';
+	updateText();
+	$( '.kittens, .person, .language' ).on( 'change keyup', updateText );
+} );
